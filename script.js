11d699784ba83d87bc484afbfa4811d6688a4900
gp208 <gustavocprieto@gmail.com>
2025-10-12 16:40:44 -0300

    open, close sidebar

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ const elements = {
 	promptContent: document.getElementById('prompt-content'),
 	titleWrapper: document.getElementById('title-wrapper'),
 	contentWrapper: document.getElementById('content-wrapper'),
+	btnOpen: document.getElementById('btn-open'),
+	btnCollapse: document.getElementById('btn-collapse'),
+  sidebar: document.querySelector('.sidebar'),
 }
 // Atualiza o estado do wrapper baseado no conteúdo do elemento
 function updateEditableWrapperState(element, wrapper) {
 	const hasContent = element.textContent.trim().length > 0
   wrapper.classList.toggle('is-empty', !hasContent)
+}
+// Abre e fecha a sidebar
+function openSidebar() {
+  elements.sidebar.style.display = 'flex'
+  elements.btnOpen.style.display = 'none'
+}
+
+function closeSidebar() {
+  elements.sidebar.style.display = 'none'
+  elements.btnOpen.style.display = 'block'
 }
 // Atualiza o estado de todos os elementos editáveis
 function updateAllEditableStates() {
@@ -29,6 +42,12 @@ function attachAllEditableHandlers() {
 function init() {
 	attachAllEditableHandlers()
 	updateAllEditableStates()
+  // Estado inicial: sidebar aberta, botão de abrir oculto
+  elements.sidebar.style.display = ''
+  elements.btnOpen.style.display = 'none'
+  // Eventos para abrir, fechar sidebar
+  elements.btnOpen.addEventListener('click', openSidebar)
+  elements.btnCollapse.addEventListener('click', closeSidebar)
 }
 
 init()
